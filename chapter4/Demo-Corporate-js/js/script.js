6b44e030e4688b1f0d1670ed937c46dd9fd58f07
Kazuya Takami <foundationmeister@outlook.com>
2021-09-21 09:19:56 +0900

    2021.09.20 commit.

---
+++ chapter4/Demo-Corporate-js/js/script.js
@@ -0,0 +1,31 @@
+let pieData = {
+    labels: ["Webデザイナー", "Webデベロッパー"],
+    series: [14, 6]
+  };
+  
+  let pieOptions = {
+    width: "100%",
+    height: "440px"
+  };
+  
+  new Chartist.Pie(".pie-chart", pieData, pieOptions);
+  
+  
+  let barData = {
+    labels: ["2018年", "2019年", "2020年"],
+    series: [[10, 16, 29]]
+  };
+  
+  let barOptions = {
+    axisY: {
+      offset: 60,
+      scaleMinSpace: 50,
+      labelInterpolationFnc: function(value) {
+        return value + "人"
+      }
+    },
+    width: "100%",
+    height: "400px"
+  }
+  
+  new Chartist.Bar(".bar-chart", barData, barOptions);
